9cb24ff3d1c82dab042552106d715964139faf01
Kate <fukuro.s@yandex.ru>
2025-02-26 14:41:46 +0300

    tempate

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import cowsay
+
+def bullcows(guess: str, riddle: str) -> (int, int):
+    pass
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    pass
+
+
