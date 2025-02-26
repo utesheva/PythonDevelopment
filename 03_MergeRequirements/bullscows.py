c0b56d3b8c00fc06f73d7343695d72a9e9e311b8
Kate <fukuro.s@yandex.ru>
2025-02-26 16:19:22 +0300

    added function to check bulls and cows

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -2,10 +2,20 @@ import random
 import sys
 import cowsay
 
-def bullcows(guess: str, riddle: str) -> (int, int):
-    pass
+def bullscows(guess: str, riddle: str) -> (int, int):
+    bull = 0
+    cows = 0
+    guess_dict = {i:0 for i in guess}
+    riddle_dict = {i:0 for i in riddle}
+    for i, j in zip(guess, riddle):
+        if i == j:
+            bull += 1
+            continue
+        guess_dict[i] += 1
+        riddle_dict[j] += 1
+    for i in set(guess_dict.keys()).intersection(set(riddle_dict.keys())):
+        cows += min(guess_dict[i], riddle_dict[i])
+    return bull, cows
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     pass
-
-
