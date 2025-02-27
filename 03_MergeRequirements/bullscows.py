0b273f382c6bd4a87132534b598501c3a23c5d53
Kate <fukuro.s@yandex.ru>
2025-02-28 00:40:44 +0300

    added reference realization of the game

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import cowsay
+import urllib.request
 
 def bullscows(guess: str, riddle: str) -> (int, int):
     bull = 0
@@ -19,6 +20,7 @@ def bullscows(guess: str, riddle: str) -> (int, int):
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     riddle = random.choice(words)
+    print(riddle)
     cnt = 0
     b = 0
     while b != len(riddle):
@@ -37,3 +39,16 @@ def ask(prompt: str, valid: list[str] = None) -> str:
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        l = int(sys.argv[2])
+    else:
+        l = 5
+    try:
+        words = urllib.request.urlopen(sys.argv[1]).read().decode().split('\n')
+    except ValueError:
+        words = open(sys.argv[1]).read().split('\n')
+    if words[-1] == '':
+        words = words[:-1]
+    print("Количество попыток:", gameplay(ask, inform, words)) 
