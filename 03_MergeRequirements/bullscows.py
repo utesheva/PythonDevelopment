37c80cef143e22b742da24f96ad1863dcfe9cd36
Kate <fukuro.s@yandex.ru>
2025-02-28 01:05:38 +0300

    fixed bug in bullcows

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -6,16 +6,13 @@ import urllib.request
 def bullscows(guess: str, riddle: str) -> (int, int):
     bull = 0
     cows = 0
-    guess_dict = {i:0 for i in guess}
-    riddle_dict = {i:0 for i in riddle}
     for i, j in zip(guess, riddle):
         if i == j:
             bull += 1
-            continue
-        guess_dict[i] += 1
-        riddle_dict[j] += 1
-    for i in set(guess_dict.keys()).intersection(set(riddle_dict.keys())):
-        cows += min(guess_dict[i], riddle_dict[i])
+    intersect = set(guess).intersection(set(riddle))
+    for i in intersect:
+        cows += min(guess.count(i), riddle.count(i))
+    cows -= bull
     return bull, cows
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
