29de784e3909d3450b9c01975cd4e8cc638af4fd
Kate <fukuro.s@yandex.ru>
2025-03-15 19:22:58 +0300

    user can't send or receive messages until login

--- 05_DiffPatchNet/cow_chat.py
+++ 05_DiffPatchNet/cow_chat.py
@@ -1,25 +1,36 @@
 import asyncio
 
+class Client:
+    def __init__(self):
+        self.cow = None
+        self.queue = asyncio.Queue() 
+
 clients = {}
 
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
     print(me)
-    clients[me] = asyncio.Queue()
+    clients[me] = Client()
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = asyncio.create_task(clients[me].queue.get())
     while not reader.at_eof():
         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
-        for q in done:
-            if q is send:
+        for q in done:  
+            if q is send and clients[me].cow is not None:
                 send = asyncio.create_task(reader.readline())
                 for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
-            elif q is receive:
+                    if out.queue is not clients[me].queue:
+                        await out.queue.put(f"{me} {q.result().decode().strip()}")
+            elif q is receive and clients[me].cow is not None:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+            elif q is send:
+                writer.write("Login first\n".encode())
+                await writer.drain()
+                send = asyncio.create_task(reader.readline())
     send.cancel()
     receive.cancel()
     print(me, "DONE")
